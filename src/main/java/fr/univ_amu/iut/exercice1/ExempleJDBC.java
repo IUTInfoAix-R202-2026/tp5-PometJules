7cca0d9bea83fd904f8db4f5f1476b1a7a3b33d0
Jules POMET <julespometpro@gmail.com>
2026-06-03 12:27:50 +0000

    exercice1 finit

--- src/main/java/fr/univ_amu/iut/exercice1/ExempleJDBC.java
+++ src/main/java/fr/univ_amu/iut/exercice1/ExempleJDBC.java
@@ -72,7 +72,13 @@ public class ExempleJDBC {
     // 2. Exécuter le SELECT : st.executeQuery("SELECT code, nom_vernaculaire FROM taxon").
     // 3. Parcourir le ResultSet avec while (rs.next()) et, pour chaque ligne, ajouter à `lignes`
     //    la chaîne : rs.getString("code") + " - " + rs.getString("nom_vernaculaire").
-
+    try (Statement st = connexion.createStatement();
+        ResultSet rs = st.executeQuery("SELECT code, nom_vernaculaire FROM taxon")) {
+      // Tant qu'il reste une ligne, on la lit et on la transforme en "code - nom".
+      while (rs.next()) {
+        lignes.add(rs.getString("code") + " - " + rs.getString("nom_vernaculaire"));
+      }
+    }
     return lignes;
   }
 }
